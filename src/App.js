59f3b98c79220e8f02afec32810e7122d7986a7d
GrayGillis <graygillis15@gmail.com>
2021-08-05 14:57:31 -0700

    changed api

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React, {useState, useEffect} from 'react'
 import './App.css';
 import Header from './Header';
 
-// web api for city weather
-// OpenWeatherApp -> get key
-const url = '';
+const api = {
+  key: '650398dcf7dcf463b2c433c49bed5580',
+  url: 'api.openweathermap.org/data/2.5/'
+}
 
 const getWeatherInfo = async () => {
-  // const response = await fetch(url);
-  // const data = await response.json();
-  // return data;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
 
 function App() {
   
-  // will be for displaying the city's weather
   const [cityWeather, setCityWeather] = useState([]);
 
   useEffect(() => {
@@ -25,6 +26,7 @@ function App() {
     <form className="the-form"> 
       <Header/>
       <div className="App">
+        
         <input type="text" name="weather" id="weather-text"/>
         <button className="btn" onClick={getWeatherInfo}>Submit</button>
       </div> 

2e22280e1dfa38457e43f0755189cbd05951e8b9
GrayGillis <graygillis15@gmail.com>
2021-08-05 20:35:20 -0700

    check

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './index.css';
 
 const Header = () => {
     return (
         <div className="header">
             <h1>Welcome to Grayson's Weather App</h1>
             <h4>This project was built on React</h4>
-            <p>Please enter the City Name & Country Code:</p>
+            <p>Please enter the City & Country:</p>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+import React, { useState } from "react";
+import { render } from "react-dom";
+import axios from "axios";
+import './index.css';
+import Header from "./Header";
+
+const api = {
+  key: '650398dcf7dcf463b2c433c49bed5580',
+  url: 'http://api.openweathermap.org/data/2.5/'
+}
+
+const ReactWeatherApp = () => {
+  const [temperatureFah, setTemperatureFah] = useState('');
+  const [temperatureCel, setTemperatureCel] = useState('');
+  const [city, setCity] = useState('London');
+  const [country, setCountry] = useState('UK');
+  const [description, setDescription] = useState('');
+  
+  const getWeatherInfo = (city, country) => {
+     axios({
+      method: "GET",
+      url: `${api.url}weather?q=${city},${country}&APPID=${api.key}`,
+    })
+      .then((res) => {
+        console.log(res.data.main.temp);
+        setTemperatureFah((res.data.main.temp - 273.15) * 1.8 + 32);
+        setTemperatureCel(res.data.main.temp - 273.15);
+        setDescription(res.data.weather[0].main);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  return (
+  <>
+      <div className="header-div">
+        <Header />
+      </div>
+      <br />
+      <div className="main-div">
+        <div>
+          {new Date().toLocaleString()}
+          <br />
+          {city} Weather
+          <br />
+          {Math.round(temperatureCel * 100) / 100} ℃ <br />
+          {Math.round(temperatureFah * 100) / 100} ℉ <br />
+          {description}
+        </div>
+        <br />
+        <input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
+        <input type="text" value={country} onChange={(e) => setCountry(e.target.value)} />
+        <button onClick={() => { getWeatherInfo(city, country) }}> Submit </button>
+      </div>
+    </>
+  );
+};
+
+render(<ReactWeatherApp />, document.querySelector("#root"));
